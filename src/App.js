07ecf23d3c916f649698af329e72016e4ec11678
Tanikonda Ramakrishna <thanikondadhanram951@gmail.com>
2020-07-01 11:11:06 +0530

    add hoc withChangeLang

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { I18nextProvider } from "react-i18next";
 
 import i18n from "./i18n";
 import NavigatingButton from "./components/NavigatingButton/NavigatingButton";
-import ChangeLangRoute from "./routes/ChangeLangRoute";
 
 const SampleRoute = lazy(() => import("./routes/SampleRoute"));
 const TestRoute = lazy(() => import("./routes/TestRoute"));
